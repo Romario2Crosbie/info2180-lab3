5a1bcd42736fc5ccb9ba7420edfc720ad293455f
Romario2Crosbie <romario.anonymous2000@gmail.com>
2022-10-10 10:43:29 -0500

    hover function added

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,6 +3,17 @@ window.onload = function() {
     const player_1 = "X"
     const player_2 = "O"
     let currentPlayer = player_1
+    let winComb = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+
     
     
     grid.childNodes.forEach(element => {
@@ -15,14 +26,21 @@ window.onload = function() {
             grd.innerText = currentPlayer
             if(currentPlayer == player_1){
                 grd.classList.add("X")
-                console.log(grd)
             } else {
                 grd.classList.add("O")
             }
             currentPlayer = currentPlayer == player_1 ? player_2 : player_1
-           
+           })
 
+        grd.addEventListener("mouseover", function handleMouseOver(){
+            grd.classList.add("hover")
         })
+
+        grd.addEventListener("mouseout" , function handleMouseOut(){
+            grd.classList.remove("hover")
+        }) 
+
+        
     })
   
 
