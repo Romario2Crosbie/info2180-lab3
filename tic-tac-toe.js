7d665596f9afd2a5f73039d1891388b5784f29ac
Romario2Crosbie <romario.anonymous2000@gmail.com>
2022-10-09 19:19:29 -0500

    layout set

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,22 @@
-ECHO is on.
+window.onload = function() {
+    let grid = document.getElementById("board");
+    console.log(grid.childNodes);
+    grid.childNodes.forEach(element => {
+        element.className = "square";
+    });
+        
+    const O_TEXT = "O"
+    const X_TEXT = "X"
+
+    const startGame = () => {
+        grid.childNodes.forEach(grd => grd.addEventListener('click', boxClicked))
+    }
+
+    function boxClicked(e){
+        console.log(e.target)
+    }
+
+    startGame()
+
+    
+}
